932043e6bbf0c123c128cd9380871975d70d0da6
IsaacKuria2 <isaackuria68@gmail.com>
2026-05-18 19:09:52 +0100

    message

--- Home.py
+++ Home.py
@@ -1,13 +1,6 @@
 import streamlit as st
 from utils.sidebar_comments import render_comment_sidebar
 
-with st.sidebar:
-    st.page_link("Home.py", label="🏠 Home")
-    st.page_link("pages/1_EDA.py", label="🔍 EDA")
-    st.page_link("pages/2_Model_Performance.py", label="📊 Model Performance")
-    st.page_link("pages/3_Live_Prediction.py", label="🧪 Live Prediction")
-    st.page_link("pages/4_Admin.py", label="🔐 Admin")
-
 st.set_page_config(
     page_title="Breast Cancer Early Detection",
     page_icon="💗",                    # Changed to emoji for reliability

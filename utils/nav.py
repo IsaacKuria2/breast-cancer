49a826d35afabd67605fa019392a5c791f043e38
IsaacKuria2 <isaackuria68@gmail.com>
2026-05-18 19:20:16 +0100

    message

--- utils/nav.py
+++ utils/nav.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
 def render_nav():
-    with st.sidebar:
-        st.markdown("## 💗 Menu")
-        st.page_link("Home.py",                      label="🏠 Home")
-        st.page_link("pages/1_EDA.py",               label="🔍 EDA")
-        st.page_link("pages/2_Model_Performance.py", label="📊 Model Performance")
-        st.page_link("pages/3_Live_Prediction.py",   label="🧪 Live Prediction")
-        st.page_link("pages/4_Admin.py",             label="🔐 Admin")
-        st.markdown("---")
+    st.sidebar.markdown("## 💗 Menu")
+    st.sidebar.page_link("Home.py",                      label="🏠 Home")
+    st.sidebar.page_link("pages/1_EDA.py",               label="🔍 EDA")
+    st.sidebar.page_link("pages/2_Model_Performance.py", label="📊 Model Performance")
+    st.sidebar.page_link("pages/3_Live_Prediction.py",   label="🧪 Live Prediction")
+    st.sidebar.page_link("pages/4_Admin.py",             label="🔐 Admin")
+    st.sidebar.markdown("---")
